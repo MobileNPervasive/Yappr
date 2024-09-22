d9346f667a728e070b0989b8c5b77742e2c656b2
nguyenthucbao <21520617@gm.uit.edu.vn>
2024-09-22 23:08:40 +0700

    feat: Add chat screen/chat box

--- src/app/pages/home/chat/chat.page.ts
+++ src/app/pages/home/chat/chat.page.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatPage implements OnInit {
 
+  name: string = 'Sender';
+  message: string = '';
+  isLoading = false;
+  currentUserId = 1;
+  chats = [
+    {id: 1, sender: 1, message: 'Hello', time: '12:00'},
+    {id: 2, sender: 2, message: 'Hi', time: '12:01'},
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  sendMessage() {}
 
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { ModalController, PopoverController } from '@ionic/angular';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,15 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
+  @ViewChild('new_chat') modal: ModalController;
+  @ViewChild('popover') popover: PopoverController;
   segment = 'chats';
+  open_new_chat = false;
+  users = [
+    {id: 1, name: 'Diggy', photo: 'https://ionicframework.com/docs/demos/api/list/avatar-finn.png'},
+    {id: 2, name: 'JustInBeber', photo: 'https://ionicframework.com/docs/demos/api/list/avatar-finn.png'}, 
+  ];
 
-  constructor() { }
+  chatRooms = [
+    {id: 1, name: 'Diggy', photo: 'https://ionicframework.com/docs/demos/api/list/avatar-finn.png'},
+    {id: 2, name: 'JustInBeber', photo: 'https://ionicframework.com/docs/demos/api/list/avatar-finn.png'},
+  ]
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    console.log('HomePage initialized');
+  }
+
+  logout() {
+    this.popover.dismiss();
   }
 
   segmentChanged(event: any) {
     console.log('Segment changed to', event);
   }
 
+  newChat() {
+    this.open_new_chat = true;
+  }
+
+  cancel() {
+
+    this.open_new_chat = false;
+  }
+
+  onWillDismiss(event: any) {}
+
+  startChat(item) {}
+
+  getChat(item) {
+    this.router.navigate(['/', 'home', 'chats', item?.id]);
+  }
 }
